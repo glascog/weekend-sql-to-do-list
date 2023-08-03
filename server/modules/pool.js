27d77ed46ba4bb1f768ab1267fa4792ca2879911
Gabe Glasco <gabeglasco@gmail.com>
2023-08-03 12:07:06 -0500

    fix pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,18 +1,19 @@
 // set up pool connection
 
-const pg = require('pg') // importing the pg package from node_modules
+const pg = require("pg"); // importing the pg package from node_modules
 let pool;
 
-if(process.env.DATABASE_URL) {
-    connectionString: process.env.DATABASE_URL;
-    ssl: {
-        rejectUnauthorized: false
-    }
+if (process.env.DATABASE_URL) {
+  connectionString: process.env.DATABASE_URL;
+  ssl: {
+    rejectUnauthorized: false;
+  }
+} else {
+  pool = new pg.Pool({
+    database: "weekendtodoapp_6g08",
+    host: "localhost",
+    port: 5432,
+  });
 }
-else pool = new pg.Pool({
-    database: 'weekendtodoapp_6g08',
-    host: 'localhost',
-    port: 5432
-})
 
-module.exports = pool
+module.exports = pool;
