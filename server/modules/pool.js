941f7bf08600c71f2996b1c518e9a451bfce960a
Gabe Glasco <gabeglasco@gmail.com>
2023-08-03 12:08:54 -0500

    fix pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -4,10 +4,12 @@ const pg = require("pg"); // importing the pg package from node_modules
 let pool;
 
 if (process.env.DATABASE_URL) {
-  connectionString: process.env.DATABASE_URL;
-  ssl: {
-    rejectUnauthorized: false;
-  }
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 } else {
   pool = new pg.Pool({
     database: "weekendtodoapp_6g08",
